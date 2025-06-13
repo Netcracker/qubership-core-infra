04310ef596d47e20958aeb7e4908b59d3b771ba0
Denis Anfimov <denis.anfimov@netcracker.com>
2025-06-13 12:52:11 +0500

    CPCAP-527 renovate

--- renovate/3rd-party-config.js
+++ renovate/3rd-party-config.js
@@ -8,20 +8,24 @@ module.exports = {
         // Spring
         {
             matchPackageNames: ["org.springframework.boot:spring-boot-dependencies"],
-            allowedVersions: "~3.4.0"
+            allowedVersions: "~3.4.0",
+            groupName: "Spring"
         },
         {
             matchPackageNames: ["org.springframework.cloud:spring-cloud-dependencies"],
-            allowedVersions: "~2024.0.0"
+            allowedVersions: "~2024.0.0",
+            groupName: "Spring"
         },
         {
             matchPackageNames: ["org.springframework.data:spring-data-bom"],
-            allowedVersions: "~2024.1.0"
+            allowedVersions: "~2024.1.0",
+            groupName: "Spring"
         },
         // Quarkus
         {
             matchPackageNames: ["io.quarkus:quarkus-bom"],
-            allowedVersions: "~3.15.0"
+            allowedVersions: "~3.15.0",
+            groupName: "Quarkus"
         },
     ],
 };
